a6cfbc670f8f5fd0249d499741906e2b637a4b45
flaaim <flaaim@list.ru>
2025-06-29 12:00:27 +1000

    update oauth.php file

---
+++ config/common/oauth.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Aego\OAuth2\Client\Provider\Yandex;
+use League\OAuth2\Client\Provider\Google;
+use Psr\Container\ContainerInterface;
+
+return [
+    'oauth.config' => [
+        'yandex' => [
+            'clientId'     => $_ENV['YANDEX_CLIENT_ID'],
+            'clientSecret' => $_ENV['YANDEX_CLIENT_SECRET'],
+            'redirectUri'  => $_ENV['YANDEX_REDIRECT_URI'],
+        ],
+        'google' => [
+            'clientId'     => $_ENV['GOOGLE_CLIENT_ID'],
+            'clientSecret' => $_ENV['GOOGLE_CLIENT_SECRET'],
+            'redirectUri'  => $_ENV['GOOGLE_REDIRECT_URI'],
+        ],
+    ],
+    Yandex::class => function (ContainerInterface $container) {
+        $config = $container->get('oauth.config')['yandex'];
+        return new Yandex($config);
+    },
+    Google::class => function (ContainerInterface $container) {
+        $config = $container->get('oauth.config')['google'];
+        return new Google($config);
+    }
+
+];
