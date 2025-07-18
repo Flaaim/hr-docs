b64041b7984d65dfd1f9a3d8907c4f0e9f5fdebf
Flaaim <flaaim@list.ru>
2025-07-18 12:14:25 +1000

    added fix deploy errorc and Sitemap.php

--- deploy.php
+++ deploy.php
@@ -5,8 +5,6 @@ require 'recipe/symfony.php';
 
 set('application', 'kd-docs');
 set('git_ssh_command', 'ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa');
-// Или используйте ssh-agent
-//set('forward_agent', true);
 set('repository', 'git@github.com:flaaim/hr-docs.git');
 set('php_version', '8.1');
 set('bin/php', '/opt/php/8.1/bin/php');
@@ -16,21 +14,10 @@ host('production')
     ->set('hostname', '31.31.198.114')
     ->set('port', 22)
     ->set('remote_user', 'u1656040')
-    ->set('password', 'OHFvqDac7O8g3RB1')
     ->set('deploy_path', '~/www/kd-docs.ru')
     ->set('public_path', '{{deploy_path}}/public')
     ->set('branch', 'master');
 
-// Кастомная задача для symlinks
-task('deploy:symlink', function () {
-    // Стандартный symlink current -> releases/N
-    run("cd {{deploy_path}} && ln -sfn {{release_path}} current");
-
-    // Дополнительный symlink для public (если требуется)
-    run("cd {{deploy_path}} && ln -sfn current/public public");
-});
-
-// Настройки Slim
 set('shared_files', [
     'public/.htaccess',
     'public/robots.txt',
@@ -40,7 +27,6 @@ set('shared_dirs', [
     'config/common/env',
     'public/uploads',
     'var',
-
 ]);
 
 set('writable_dirs', [
@@ -48,27 +34,24 @@ set('writable_dirs', [
     'public/uploads',
 ]);
 
-set('bin/composer', '{{bin/php}} /var/www/u1656040/data/www/kd-doc.ru/composer.phar');
-
+// Updated composer path to use deploy_path
+set('bin/composer', '{{bin/php}} {{deploy_path}}/composer.phar');
 set('composer_options', '--no-dev --optimize-autoloader --no-progress --no-interaction --no-scripts');
+
+// Improved composer check task
+task('check:composer', function () {
+    if (!test('[ -f {{deploy_path}}/composer.phar ]')) {
+        run('cd {{deploy_path}} && curl -sS https://getcomposer.org/installer | {{bin/php}}');
+        run('cd {{deploy_path}} && mv composer.phar {{deploy_path}}/composer.phar');
+    }
+});
 
 task('deploy:vendors', function () {
     run('cd {{release_path}} && {{bin/composer}} install {{composer_options}}');
 });
 
-// Дополнительная проверка
-task('check:composer', function () {
-    run('if [ ! -f {{deploy_path}}/composer.phar ]; then curl -sS https://getcomposer.org/installer | {{bin/php}} && mv composer.phar {{deploy_path}}/; fi');
-});
-
-
-before('deploy:vendors', 'check:composer');
-
 task('deploy:symlink', function () {
-    // Создаем стандартный симлинк current -> releases/N
     run("cd {{deploy_path}} && ln -sfn {{release_path}} current");
-
-    // Дополнительно: симлинк для public (если нужно)
     run("cd {{deploy_path}} && ln -sfn current/public public_html");
 });
 
@@ -79,4 +62,5 @@ task('deploy', [
     'deploy:publish'
 ]);
 
+before('deploy:vendors', 'check:composer');
 after('deploy:failed', 'deploy:unlock');
