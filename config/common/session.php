79ccd275b8f0004c055afc9325373179e91e9240
Flaaim <flaaim@list.ru>
2025-09-20 15:01:59 +1000

    added php-cli session

--- config/common/session.php
+++ config/common/session.php
@@ -4,22 +4,25 @@ declare(strict_types=1);
 
 use Odan\Session\PhpSession;
 use Odan\Session\SessionInterface;
-use Psr\Container\ContainerInterface;
 
 return [
     SessionInterface::class => function () {
-        $session = new PhpSession();
-        $session->setOptions([
-            'name' => 'hr-docs',
-            'lifetime' => 7200,
-            'secure' => false,
-            'httponly' => true,
-            'samesite' => 'Lax',
-        ]);
-        if(session_status() !== PHP_SESSION_ACTIVE){
-            $session->start();
+
+        if(php_sapi_name() !== 'cli'){
+            $session = new PhpSession();
+            $session->setOptions([
+                'name' => 'hr-docs',
+                'lifetime' => 7200,
+                'secure' => false,
+                'httponly' => true,
+                'samesite' => 'Lax',
+            ]);
+            if(session_status() !== PHP_SESSION_ACTIVE){
+                $session->start();
+            }
+            return $session;
         }
 
-        return $session;
+        return new PhpSession();
     }
 ];
