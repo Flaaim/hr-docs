c7cd45a7eb15bb46d5f78f6dcf7404ec08cae539
Flaaim <flaaim@list.ru>
2025-09-24 08:28:08 +1000

    added mailing unsubscribe

--- src/Http/Mailing/routes.php
+++ src/Http/Mailing/routes.php
@@ -13,5 +13,11 @@ use Slim\Routing\RouteCollectorProxy;
 $app->group('/api/mailing', function (RouteCollectorProxy $group){
     $group->get('/list', [MailingController::class, 'list']);
     $group->post('/send', [MailingController::class, 'send']);
+
+
+});
+
+$app->group('/mailing', function (RouteCollectorProxy $group){
+    $group->get('/unsubscribe', [MailingController::class, 'unsubscribe']);
 });
 
